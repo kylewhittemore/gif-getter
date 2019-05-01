9bff373e9166702c40c7967cb1da316fb66e1b71
KDW <kyle.whittemore@gmail.com>
2019-05-01 15:25:24 -0400

    basic keyword search

--- gif-getter.js
+++ gif-getter.js
@@ -1,39 +1,24 @@
-const topicsArray = ["The Office", "The Simpsons", "Family Matters", "Married with Children", "Seinfeld", "Curb Your Enthusiasm"];
+$('#custom-search').on('click', function () {
+    let searchObject = {
+        searchWord: $("#search-text").val().trim(),
+        limit: $('#limit-return').val(),
+        offset: 0,
+        rating: '',
+    };
 
-$
-
-
-$("#add-topic").on("click", function (event) {
-    event.preventDefault();
-    let topic = $("#topic-input").val().trim();
-    topicsArray.push(topic);
-    $("form").trigger("reset");
-    renderButtons();
+    getGifs(searchObject);
 });
 
-function renderButtons() {
-    $("#button-container").empty();
-    for (i = 0; i < topicsArray.length; i++) {
-        let button = $(`<button type="button" class="btn m-1 btn-success">`);
-        button.attr("data-topic", topicsArray[i]);
-        button.attr("data-offset", 0)
-        button.text(topicsArray[i]);
-        $("#button-container").append(button);
-    }
-    $("button").on("click", getGifs);
-};
-
-function getGifs() {
-    let topic = $(this).attr("data-topic");
-    let offset = parseInt($(this).attr("data-offset"));
-    console.log(offset);
-    console.log(topic);
+function getGifs(queryObject) {
+    let topic = queryObject.searchWord;
+    let offset = queryObject.offset;
+    let limit = parseInt(queryObject.limit);
 
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&limit=10&offset=" + offset + "&api_key=G56yi3BK55QO0wjAO1tKVY2Vy4xkDTZH";
+        topic + "&limit=" + limit + "&offset=" + offset + "&api_key=G56yi3BK55QO0wjAO1tKVY2Vy4xkDTZH";
 
     offset = offset + 10;
-    $(this).attr("data-offset", offset)
+    queryObject.offset = offset;
 
     $.ajax({
         url: queryURL,
@@ -53,5 +38,3 @@ function getGifs() {
         }
     });
 };
-
-renderButtons();
