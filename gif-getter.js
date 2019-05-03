98d48ad986c7c9284539dc2e972a4430874fdce8
KDW <kyle.whittemore@gmail.com>
2019-05-03 12:44:27 -0400

    working off of index2; navbar control re-factor

--- gif-getter.js
+++ gif-getter.js
@@ -1,5 +1,5 @@
 
-const topicsArray = ["The Office", "The Simpsons", "Family Matters", "Married with Children", "Seinfeld", "Curb Your Enthusiasm"];
+const topicsArray = [];
 
 $('#custom-search').on('click', function (event) {
     event.preventDefault();
@@ -12,7 +12,7 @@ $('#custom-search').on('click', function (event) {
     let keepSearch = $('#keep-search').is(":checked");
 
     if (keepSearch) {
-        let topic = searchObject.searchWord;
+        let topic = [searchObject.searchWord, searchObject.limit];
         console.log(topic);
         topicsArray.push(topic);
         renderButtons();
@@ -26,15 +26,19 @@ $('#custom-search').on('click', function (event) {
 function renderButtons() {
     $("#button-container").empty();
     for (i = 0; i < topicsArray.length; i++) {
-        let button = $(`<button type="button" class="btn m-1 btn-success">`);
-        button.attr("data-topic", topicsArray[i]);
+        let button = $(`<button type="button" class="dropdown-item">`);
+        button.attr("data-topic", topicsArray[i][0]);
         button.attr("data-limit", 10);
-        button.attr("data-offset", 0);
+        button.attr("data-offset", topicsArray[i][1]);
         button.attr("data-rating", "g");
-        button.text(topicsArray[i]);
+        button.text(topicsArray[i][0]);
         $("#button-container").append(button);
     }
     $("button").on("click", function () {
+        let offset = parseInt($(this).attr("data-offset"));
+        let limit = parseInt($(this).attr("data-limit"));
+        console.log("offset: " + offset);
+        console.log("limit: " + limit);
         let searchObject = {
             searchWord: $(this).attr("data-topic"),
             limit: $(this).attr("data-limit"),
@@ -42,6 +46,8 @@ function renderButtons() {
             rating: $(this).attr("data-rating")
         };
         getGifs(searchObject);
+        offset = offset + limit;
+        $(this).attr("data-offset", offset);
     });
 };
 
@@ -51,18 +57,15 @@ function getGifs(queryObject) {
     let offset = queryObject.offset;
     let limit = parseInt(queryObject.limit);
     let rating = queryObject.rating;
-    
+
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topic + "&limit=" + limit + "&offset=" + offset + "&rating=" + rating + "&api_key=G56yi3BK55QO0wjAO1tKVY2Vy4xkDTZH";
-    
-    offset = offset + 10;
-    queryObject.offset = offset;
-    
+        topic + "&limit=" + limit + "&offset=" + offset + "&rating=" + rating + "&api_key=G56yi3BK55QO0wjAO1tKVY2Vy4xkDTZH";
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        
+
         let results = response.data;
         // Looping through each result item
         for (i = 0; i < results.length; i++) {
